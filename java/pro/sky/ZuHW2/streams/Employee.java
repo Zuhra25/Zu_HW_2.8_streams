e6f9bdaf88cd0a9f0f5f4a5575858c82108d5298
ZU <zshaihova@gmail.com>
2023-07-23 01:28:05 +0300

    начальный коммит

---
+++ java/pro/sky/ZuHW2/streams/Employee.java
@@ -0,0 +1,69 @@
+package pro.sky.ZuHW2.streams;
+
+import java.util.Objects;
+
+public class Employee {
+    public String firstName;
+    public String lastName;
+    public double salary;
+    public int department;
+
+
+
+    public Employee(String firstName, String lastName, double salary, int department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName; }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary='" + salary + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+}
